fafb7f2ef1b7495b1dd0bcec5b06fb996e67ef6b
DTshiyeya <164188333+DTshiyeya@users.noreply.github.com>
2025-11-02 22:58:57 +0200

    i have connected the back to dashboard button to patientdashboard

--- src/gui/BookAppointment.java
+++ src/gui/BookAppointment.java
@@ -218,6 +218,8 @@ public class BookAppointment extends javax.swing.JFrame {
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
+        new PatientDashboard().setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
